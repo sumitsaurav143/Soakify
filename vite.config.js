630a49c3ec907b0274fd3783964a78b0082fb64a
Sumit <sumit@Sumits-Mac.local>
2025-01-02 16:50:26 +0530

    Update Homepage

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://sumitsaurav143.github.io/Soakify/', 
   server: {
     host: true, // Allows external devices to connect
     port: 5173, // You can specify a port if needed
